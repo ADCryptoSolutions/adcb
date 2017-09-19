4a7cc65dd6bb5a5c91b3179daf98213ff8954f29
dsiervo <emetdan@gmail.com>
2017-09-19 16:24:14 -0500

    agregando EMA, SMA y calculando vector de pesos W. Aún no es claro como calcular el profit de una determinada estrategia

---
+++ orders.py
@@ -0,0 +1,20 @@
+#-*-coding:utf-8-*-
+import pandas as pd
+# recibe el data frame w (pesos) y a grega a este
+# la columna order con wait, sell o buy dependiendo el caso
+def orders(w):
+    w2 = pd.Series(index=w.index, dtype=object)
+    
+    for i in range(len(w)-1):
+      if w[i+1] != w[i]:
+        if w[i] == 0.5 and w[i+1] ==0.0:
+            w2[i]= "WAIT"
+            w2[i+1]= "WAIT"
+        elif w[i+1] == 1:
+            w2[i+1] = "BUY"
+        elif w[i+1] == 0:
+            w2[i+1] = "SELL"
+      else:
+        w2[i+1] = "WAIT"
+    
+    return w2

19292366f8309fc92b0b76ab0b575ac4c9796fd6
Daniel Siervo <emetdan@gmail.com>
2017-11-02 22:08:24 -0500

    Calculando RSI manualmente
    Se agrega en el nuevo módulo indicators función que calcula el RSI de dos maneras distintas: sma, ema

---
+++ indicators.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 02 18:37:54 2017
+
+@author: Daniel
+"""
+
+import pandas
+from pandas_datareader import data
+import datetime
+import matplotlib.pyplot as plt
+
+
+
+def rsi(close,w=14):
+    
+    # Window length for moving average
+    window_length = w
+    # Get the difference in price from previous step
+    delta = close.diff()
+    # Get rid of the first row, which is NaN since it did not have a previous 
+    # row to calculate the differences
+    delta = delta[1:] 
+    
+    # Make the positive gains (up) and negative gains (down) Series
+    up, down = delta.copy(), delta.copy()
+    up[up < 0] = 0
+    down[down > 0] = 0
+    
+    # Calculate the EWMA
+    roll_up1 = pandas.stats.moments.ewma(up, window_length)
+    roll_down1 = pandas.stats.moments.ewma(down.abs(), window_length)
+    
+    # Calculate the RSI based on EWMA
+    RS1 = roll_up1 / roll_down1
+    RSI1 = 100.0 - (100.0 / (1.0 + RS1))
+    
+    # Calculate the SMA
+    roll_up2 = pandas.rolling_mean(up, window_length)
+    roll_down2 = pandas.rolling_mean(down.abs(), window_length)
+    
+    # Calculate the RSI based on SMA
+    RS2 = roll_up2 / roll_down2
+    RSI2 = 100.0 - (100.0 / (1.0 + RS2))
+    
+    return RSI1,RSI2
+		
+if __name__ == "__main__":
+    # Dates
+    start = datetime.datetime(2010, 1, 1)
+    end = datetime.datetime(2013, 1, 27)
+    # Get data
+    data = data.DataReader('AAPL', 'yahoo', start, end)
+    # Get just the close
+    close = data['Adj Close']
+    
+    RSI1, RSI2 = rsi(close=close)
+    # Compare graphically
+    plt.figure()
+    RSI1.plot()
+    RSI2.plot()
+    plt.legend(['RSI via EWMA', 'RSI via SMA'])
+    plt.show()
